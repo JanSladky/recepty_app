3a62e10820e29de1d42518c21b751b5c59521a67
Jan Sladký <sladky.honza@gmail.com>
2025-05-21 15:34:16 +0200

    Fix: pridani Tailwind to frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: [],
+  darkMode: "false",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
